a713d7a28c214344ead44f13d8c9d42d58433603
Malsa <157951405+maloglolo@users.noreply.github.com>
2024-02-22 09:31:45 +0100

    modified:   .gitignore 	modified:   php-files/process-signup.php

--- php-files/process-signup.php
+++ php-files/process-signup.php
@@ -3,29 +3,22 @@ mysqli_report(MYSQLI_REPORT_OFF);
 if(empty($_POST["name"])) {
     die("Name is required");
 }
-
 if ( ! filter_var($_POST["email"], FILTER_VALIDATE_EMAIL)){
     die("Valid email is required");
 }
-
 if (strlen($_POST["password"])  < 8 ) {
     die("Password must be atleast 8 characters");
 }
-
 if ( ! preg_match("/[a-z]/i", $_POST["password"])) {
     die("Password must contain atleast one letter");
 }
-
 if ( ! preg_match("/[0-9]/", $_POST["password"])) {
     die("Password must contain atleast one number");
 }
-
 if ($_POST["password"] !== $_POST["password_confirmation"]) {
     die("Passwords must match!");
 }
-
 $password_hash = password_hash($_POST["password"], PASSWORD_DEFAULT);
-
 #user_hash test
 #Todo: convert to shorter base64 or something
 $email = $_POST['email'];
@@ -33,6 +26,8 @@ $currentTime = time();
 $randomBytes = openssl_random_pseudo_bytes(16);
 $randomComponent = bin2hex($randomBytes);
 $user_hash = hash('sha256', $email . $currentTime . $randomComponent);
+
+###$encoded_user_hash = base64_encode($user_hash);
 
 $mysqli = require __DIR__ . "/database.php";
 
@@ -55,19 +50,15 @@ $stmt = $mysqli->stmt_init();
 if ( ! $stmt->prepare($sql)) {
     die("SQL error: " . $mysqli->error);
 }
-
 $stmt->bind_param("ssss",
                     $_POST["name"],
                     $_POST["email"],
                     $password_hash,
                     $user_hash);
 if ($stmt->execute()){
-
-header("Location: signup-success.php");
+    header("Location: signup-success.php");
     exit;
-
 } else {
-
     if ($mysqli->errno === 1062) {
         die("Email already taken.");
     }
